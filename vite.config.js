6332d7884b7d656088894185a8e01f996bce7331
vagrant <vagrant@homestead>
2023-05-30 02:42:16 +0000

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,26 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    return  {
+        server: {
+            hmr: {
+            host: env.HOMESTEAD_HOST,
+            },
+            host: env.HOMESTEAD_HOST,
+            watch: {
+            usePolling: true,
+            },
+        },
+        plugins: [
+            laravel({
+                input: [
+                    'resources/sass/app.scss',
+                    'resources/js/app.js',
+                ],
+                refresh: true,
+            }),
+        ],
+    }
 });
